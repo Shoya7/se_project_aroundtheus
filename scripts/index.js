88a3cf7d948776e7463a264a7fae598c68d8cd92
Steven Haliburton <haliburton7@gmail.com>
2024-04-28 17:11:48 -0500

    clear input after sumbit

--- scripts/index.js
+++ scripts/index.js
@@ -83,6 +83,8 @@ function handleAddCardSubmit(e) {
   const name = cardTitleInput.value;
   const link = cardUrlInput.value;
   renderCard({ name, link }, cardListEl);
+  cardTitleInput.value = "";
+  cardUrlInput.value = "";
 
   closePopUp();
 }
